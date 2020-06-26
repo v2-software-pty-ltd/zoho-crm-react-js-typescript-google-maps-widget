ba50c8d25b986b21e2cf68bcecc2c203505d39fb
John Codeinaire <johncodeinaire@gmail.com>
2020-06-26 14:25:51 +1000

    Update - RE: jez feedback

--- src/components/MassMailButton.tsx
+++ src/components/MassMailButton.tsx
@@ -7,8 +7,8 @@ type MassMailButton = {
 }
 export function MassMailButton (props: MassMailButton) {
     return (
-        <div className="mail-comment-button-wrapper">
-            <button onClick={async () => {
+        <div className="download-button-wrapper">
+            <button className="side-padding" onClick={async () => {
                 await massMailResults(props.results)
                 window.open('https://crm.zoho.com/crm/org673963570/tab/CustomModule7/custom-view/3430088000003565042/list', 'Mass_Email_Window', '"menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes"')
             }}>
