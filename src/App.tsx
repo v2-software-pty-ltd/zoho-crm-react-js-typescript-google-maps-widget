3720029182fd3c12a888d75dc1572d4399f4729c
John Codeinaire <johncodeinaire@gmail.com>
2020-07-08 08:30:21 +1000

    Start - lease evidence filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './index.css'
 import { SearchWidgetWrapper } from './components/SearchWidgets'
-import { findMatchingProperties, getGoogleMapsAPIKeyFromCRM, getSearchAddressPosition } from './services/crmDataService'
+import { findMatchingRecords, getGoogleMapsAPIKeyFromCRM, getSearchAddressPosition } from './services/crmDataService'
 import { MapWidget } from './components/MapWidget'
 import { ResultsTableWidget } from './components/ResultsTable'
 import { DownloadMailingListButton } from './components/DownloadMailingListButton'
@@ -72,7 +72,7 @@ function App () {
         if (isReadyForSearch) {
             const getDataFromCrm = async () => {
                 setLoading(true)
-                const { matchedProperties, uniqueSearchRecords } = await findMatchingProperties(searchParameters)
+                const { matchedProperties, uniqueSearchRecords } = await findMatchingRecords(searchParameters, 'Properties')
                 const searchAddressPosition = await getSearchAddressPosition(searchParameters)
                 setSearchAddressPosition(searchAddressPosition)
                 setUniqueSearchRecords(uniqueSearchRecords.length)
