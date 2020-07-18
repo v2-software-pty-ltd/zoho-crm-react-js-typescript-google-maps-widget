a3e68233fa0d070c2485d13ff67b854859f793d4
Jeremy Nagel <jeremymnagel@gmail.com>
2020-07-18 12:24:31 +1000

    Clean up a few things (#18)

--- update-app-html.js
+++ update-app-html.js
@@ -35,7 +35,7 @@ processedAppHTML = processedAppHTML.replace(
 )
 
 // remove empty lines
-processedAppHTML = processedAppHTML.replace(/^[\s]+$/gm, '').replace(/[\n]+/g, '\n');
+processedAppHTML = processedAppHTML.replace(/^[\s]+$/gm, '').replace(/[\n]+/g, '\n')
 
 fs.writeFileSync(appFilePath, processedAppHTML)
 const filename = `index-v${(Math.random() * 100).toFixed(0)}.html`
