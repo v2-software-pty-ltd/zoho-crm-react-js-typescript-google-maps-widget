333ebaddc925f1f6bb207d1a58471d8ff5b312e6
deonvisser44 <deonvisser44@gmail.com>
2022-04-27 10:22:40 +0700

    Remove old files

--- src/utils/getUniqueListBy.ts
+++
@@ -1,5 +0,0 @@
-import { UnprocessedResultsFromCRM } from '../types'
-
-export default function getUniqueListBy (arr: UnprocessedResultsFromCRM[], key: string) {
-    return [...new Map(arr.map((eachPropertyObject: UnprocessedResultsFromCRM) => [eachPropertyObject[key], eachPropertyObject])).values()]
-}
