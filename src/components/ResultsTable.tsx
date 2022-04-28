365b16ddac7116431c4778489806990b72d75f4d
deonvisser44 <51429368+deonvisser44@users.noreply.github.com>
2022-04-28 16:55:11 +0700

    Switched around Property type and croup

--- src/components/ResultsTable.tsx
+++ src/components/ResultsTable.tsx
@@ -88,7 +88,7 @@ export function ResultsTableWidget (props: ResultsTableProps) {
                                  <th>Tenancy Name</th>
                                  <th>Current $ Per Sqm</th>
                                  <th>Land Area</th>
-                                 <th>Build Area</th>
+                                 <th>Area(sqm)</th>
                                  <th>Current Rent (Gross)</th>
                              </tr>
                          </thead>
@@ -101,7 +101,7 @@ export function ResultsTableWidget (props: ResultsTableProps) {
                                          <td>{result.Name}</td>
                                          <td>{convertToCurrency(result.Current_Per_Sqm)}</td>
                                          <td>{result.Land_Area_sqm}</td>
-                                         <td>{result.Build_Area_sqm}</td>
+                                         <td>{result.Area_sqm}</td>
                                          <td>{convertToCurrency(result.Current_AI_New_Market_Rental)}</td>
                                      </tr>
                                  )
