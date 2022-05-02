93fa2282349a0de8351478c7372c19b0d84780e4
deonvisser44 <51429368+deonvisser44@users.noreply.github.com>
2022-05-03 03:39:21 +0700

    CSS changes (#33)
    * CSS changes
    
    * Fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './index.css'
 import { SearchWidgetsWrapper } from './components/SearchWidgetsWrapper'
 import {
     findMatchingRecords,
