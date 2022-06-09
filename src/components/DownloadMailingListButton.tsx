b7f394eff9ee0769bf4b88a557d813ff787f3dd4
deonvisser44 <51429368+deonvisser44@users.noreply.github.com>
2022-06-09 10:11:27 +0700

    Changed mailing list format (#41)
    * CSS changes
    
    * Fix conflicts
    
    * Switched around dropdowns
    
    * Switched dropdowns
    
    * Switched around dropdowns
    
    * Switched around inputs
    
    * update Cache
    
    * Added refresh button
    
    * Fixed caching issue
    
    * Fixed mailing list
    
    * Changed mailing list format
    
    * Changed Mailinglist format
    
    * Renamed variables

--- src/components/DownloadMailingListButton.tsx
+++ src/components/DownloadMailingListButton.tsx
@@ -1,52 +1,46 @@
 import React from 'react'
-import { UnprocessedResultsFromCRM, OwnerType } from '../types'
+import { UnprocessedResultsFromCRM, IntersectedSearchAndFilterParams } from '../types'
 
 type DownloadButtonProps = {
+    searchParameters: IntersectedSearchAndFilterParams[];
     results: UnprocessedResultsFromCRM[]
 }
 export function DownloadMailingListButton (props: DownloadButtonProps) {
+    const searchedAddress = props.searchParameters[0].searchAddress
     let downloadUrl = null
     const matchingPropertiesAndOwners = props.results
     const ownerContactDupeRemoval: string[] = []
     function generateCSVRow (propertyObject: UnprocessedResultsFromCRM) {
-        let csvRow = ''
+        let csvRowsForProperty = ''
         let doNotMail
         let returnToSender
         let postalAddress
+        const propertyReverseGeocodedAddress = propertyObject.Reverse_Geocoded_Address
         const propertyAddress = propertyObject.Deal_Name
         const propertyType = propertyObject.Property_Category_Mailing.join('; ')
-        const ownerObject = propertyObject.owner_details?.filter(item => item.Company)
-        const relatedContact = ownerObject?.find((owner: OwnerType) => owner.Contact_Type === 'Director')
-        const owner = ownerObject?.find((owner: OwnerType) => owner.Contact_Type === 'Owner')
+        const ownersArray = propertyObject.owner_details
 
-        let ownerOrRelatedContact = owner || relatedContact || null
-        if (!relatedContact && owner) {
-            doNotMail = owner.Do_Not_Mail
-            returnToSender = owner.Return_to_Sender
-            postalAddress = owner.Postal_Address
-            ownerOrRelatedContact = owner
-        } else if (relatedContact) {
-            doNotMail = relatedContact.Do_Not_Mail
-            returnToSender = relatedContact.Return_to_Sender
-            postalAddress = relatedContact.Postal_Address
-            ownerOrRelatedContact = relatedContact
-        }
-
-        const isDupe = ownerContactDupeRemoval.includes(`${ownerOrRelatedContact?.Postal_Address}-${ownerOrRelatedContact?.Name}`)
-        if (!doNotMail && !returnToSender) {
-            if (propertyAddress && postalAddress) {
-                if (!isDupe) {
-                    ownerContactDupeRemoval.push(`${ownerOrRelatedContact?.Postal_Address}-${ownerOrRelatedContact?.Name}`)
-                    const lastMailed = owner?.Last_Mailed || relatedContact?.Last_Mailed || 'Last mailed has not been found'
-                    csvRow = `"${ownerOrRelatedContact?.Name}","${ownerOrRelatedContact?.Contact_Type}","${postalAddress}","${ownerOrRelatedContact?.Postal_Suburb}","${ownerOrRelatedContact?.Postal_State}","${ownerOrRelatedContact?.Postal_Postcode}","${propertyAddress}",${propertyType},${lastMailed}\r\n`
-                    csvRow = csvRow.replace(/null/g, '-')
+        const isExactMatchForSearchAddress = propertyReverseGeocodedAddress.includes(searchedAddress)
+        ownersArray.forEach(function (arrayItem) {
+            doNotMail = arrayItem.Do_Not_Mail
+            returnToSender = arrayItem.Return_to_Sender
+            postalAddress = `${arrayItem.Postal_Street_No} ${arrayItem.Postal_Street}, ${arrayItem.Postal_Suburb}`
+            const isDupe = ownerContactDupeRemoval.includes(`${postalAddress}-${arrayItem?.Name}`)
+            if (!doNotMail && !returnToSender) {
+                if (!postalAddress.includes('null') && !isExactMatchForSearchAddress) {
+                    if (!isDupe) {
+                        const lastMailed = arrayItem.Last_Mailed || 'Last mailed has not been found'
+                        ownerContactDupeRemoval.push(`${postalAddress}-${arrayItem?.Name}`)
+                        csvRowsForProperty += `"${propertyAddress}","${arrayItem?.Company}","${arrayItem?.Name}","${arrayItem?.First_Name}","${arrayItem?.Last_Name}","${postalAddress}","${arrayItem?.Postal_Suburb}","${arrayItem?.Postal_State}","${arrayItem?.Postal_Postcode}","${arrayItem?.Salutation_Dear}",${propertyType},${lastMailed}\r\n`
+                        csvRowsForProperty = csvRowsForProperty.replace(/null/g, '-')
+                    }
                 }
             }
-        }
-        return csvRow
+        })
+        return csvRowsForProperty
     }
 
-    const HEADER_ROW = 'Contact Name,Contact Type,Mailing Street Address,Mailing Suburb,Mailing State,Mailing Postcode,Property Address,Property Type (Marketing),Last Mailed\r\n'
+    const HEADER_ROW = 'Property Address,Owner - Name On Title,Contact Name,First Name,Last Name,Mailing Street Address,Mailing Suburb,Mailing State,Mailing Postcode,Salutation,Property Type (Marketing),Last Mailed\r\n'
     const csvRows = matchingPropertiesAndOwners.map(generateCSVRow).join('')
     const csvData = `${HEADER_ROW}${csvRows}`
     const resultsBlob = new Blob(
